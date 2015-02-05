65bfb3cb7c35e14ea4e95cf7543b357fd7400e2d
bibiana <bcristofol@gmail.com>
2015-02-05 16:54:00 +0000

    Added more jquery functionality

--- load_user.js
+++ load_user.js
@@ -1,6 +1,22 @@
-$(document).ready(function(){
-  $.get('https://api.github.com/users/tansaku',function(user){
+function addProfileFromUsername(username) {
+  $.get('https://api.github.com/users/'+ username, function(user){
     var newProfile = Mustache.render($('#profile-template').html(),user);
-    $('.profile-container').append(newProfile);
+    $(newProfile).appendTo('.profile-container').slideDown();
+  }).error(function() {
+    alert('No such user with the username: ' + username);
   });
+}
+
+$(document).ready(function(){
+  $('#add_profile').on('submit', function(event) {
+    event.preventDefault();
+    addProfileFromUsername($('#username').val());
+  });
+  $('.profile-container').on('click', '.close', function() {
+    $(this).parent().slideUp(function() {
+      $(this).remove();
+    });
+    
+  });
+
 });
