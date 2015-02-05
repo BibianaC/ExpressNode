f1f27d05ece2fcd3cdd2f0a858ef9cb9f7f1211e
bibiana <bcristofol@gmail.com>
2015-02-05 16:27:21 +0000

    Fixed bug

--- load_user.js
+++ load_user.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
-  $.get('https://api.github.com/users/marcinwal',function(user){
+  $.get('https://api.github.com/users/tansaku',function(user){
     var newProfile = Mustache.render($('#profile-template').html(),user);
     $('.profile-container').append(newProfile);
-  })
+  });
 });
-
-// $(document).ready(function(){
-//   $.get('https://localhost:9999/users/tansaku',function(user){
-//     var newProfile = Mustache.render($('#profile-template').html(),user);
-//     // $('.profile-container').append(newProfile);
-//   })
-// });
