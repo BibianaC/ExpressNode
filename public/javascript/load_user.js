5bf1e25a8b389148da8d461c4d158f6eca5580fc
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-06 10:33:47 +0000

    working final version with the spinner!

--- public/javascript/load_user.js
+++ public/javascript/load_user.js
@@ -1,10 +1,23 @@
 function addProfileFromUsername(username) {
+  $('.spinner').show();
+
   $.get('https://api.github.com/users/'+ username, function(user){
     var newProfile = Mustache.render($('#profile-template').html(),user);
     $(newProfile).appendTo('.profile-container').slideDown();
+
+    
+
   }).error(function() {
-    alert('No such user with the username: ' + username);
-  });
+    // alert('No such user with the username: ' + username);
+    $('.modal').addClass('shown');
+
+    $('.modal .close').on('click',function(){
+      $('.modal').removeClass('shown');
+    });
+  }).always(function(){
+      $('#username').val('');
+      $('.spinner').css({display: 'none'});
+    });
 }
 
 $(document).ready(function(){
